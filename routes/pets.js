76ba0d10c1a959aee6ace7923058988fdfbeb1b6
scottzyang <scottzyang@gmail.com>
2023-05-24 16:00:32 -0500

    Implemented search pagination

--- routes/pets.js
+++ routes/pets.js
@@ -60,11 +60,16 @@ module.exports = (app) => {
   app.get('/search', (req, res) => {
     const term = new RegExp(req.query.term, 'i')
 
-    Pet.find({$or:[
-      {'name': term},
-      {'species': term}
-    ]}).exec((err, pets) => {
-      res.render('pets-index', { pets: pets });
-    })
+    const page = req.query.page || 1
+    Pet.paginate(
+      {
+        $or: [
+          { 'name': term },
+          { 'species': term }
+        ]
+      },
+      { page: page }).then((results) => {
+        res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page, term: req.query.term });
+      });
   });
 }
