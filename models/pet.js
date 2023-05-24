1764f165eb14abc19affbbe096879ba6568c9817
scottzyang <scottzyang@gmail.com>
2023-05-24 15:31:35 -0500

    Implemented pagination on the home page

--- models/pet.js
+++ models/pet.js
@@ -1,7 +1,12 @@
 "use strict";
-
 const mongoose = require('mongoose'),
         Schema = mongoose.Schema;
+
+const mongoosePaginate = require('mongoose-paginate');
+
+mongoosePaginate.paginate.options = {
+  limit: 3
+}
 
 const PetSchema = new Schema({
     name            : { type: String, required: true }
@@ -15,5 +20,7 @@ const PetSchema = new Schema({
 {
   timestamps: true
 });
+
+PetSchema.plugin(mongoosePaginate);
 
 module.exports = mongoose.model('Pet', PetSchema);
